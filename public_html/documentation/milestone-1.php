bd81e1b96be8e41db3733992fb72572f5adc9084
Robbie Lopez <rolopez.email@gmail.com>
2017-05-15 08:50:58 -0600

    added images to pwp

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,10 +1,16 @@
-<!DOCTYPE html>
+<!doctype html>
 
 
-<HTML>
+<html>
 
 
 	<head>
+
+		<meta charset="utf-8">
+
+		<title>Robbie Lopez</title>
+
+
 
 
 	</head>
@@ -19,13 +25,62 @@
 
 
 		<main>
-			Hello World 2
+
+			<h1>PWP Milestone 1</h1>
+
+			<h2>Purpose, Audience, & Goal</h2>
+
+			<h3>Purpose:</h3>A professional and personal website that delivers a great first impression of my unique entrepreneurial skill set.<br>
+
+			<h3>Audience:</h3> I will be targeting potential clients that are in need of web development and their business needs. <br>
+
+			<h3>Goal:</h3> To develop myself into a profitable product that consumers can relate to and trust.<br>
+
+
+			<h2>Persona</h2>
+
+			<h3>Name:</h3> Nam Giang<br>
+
+			<h3>Age:</h3>39<br>
+
+			<h3>Profession:</h3> He recently resigned from his last job as customer service manager at Comcast. Now he does what he loves, and opened up a small
+			pho restaurant called Pho King Good in Nob Hill. Looking to hire a cook and waitress.<br>
+
+			<h3>Technology:</h3> Windows user with basic computer skills. He has a Samsung Galaxy 7 with an office at home and at work.<br>
+
+			<h3>Attitudes & Behaviors:</h3> Due to needing low over head Nam always has to be helping out at work. So he has no time for doing extra stuff out of
+			work. Always on the go and probably stresses himself out too much. Has all the basic social media sites but is rarely on them and the profiles need to be updated.<br>
+
+			<h3>Frustrations & Needs: </h3> A lot of competition with a pho restaurant on every corner, no background in marketing or accounting. Hard to maintain an employee due to servers
+			salary and hiring a cook that has special cooking skills that will take a lower than usual salary for that type of work; only hires through craigslist and usually hires the
+			first person he gets an email from.<br>
+
+			<h3>Goals:</h3> Get a well developed website so customers can see the food services and products they have to offer. Since pho has such a different taste and look new customers
+			need to be educated and intrigued about the product. Start a marketing campaign at a low cost. Develop better hiring practices with more candidates. Update social medias with
+			updated information about Nam and Pho King Good. Maybe adopt a new employee strategy or hire a someone with managing background or both!<br>
+
+			<h3>User Story:</h3> As a user, I am looking for someone with good general business practices to make me and my business profitable and letting me enjoy
+			my business and not stress over it.<br>
+
+
+			<h2>Use Case</h2> Nam has had his small business pho restaurant operating for 3 months now. He really does not have much experience in this area. At his old job he used to talk
+			to customers over the phone and manage other employees that were talking to other customers on the phone. At the moment his business is not starting out on the right foot. He is not
+			seeing the customer base he thought he would get right away, which in turn makes it hard to keep employees. A server needs customers and also needs customers to pay a cook that
+			wants to be paid for being able to make such a unique taste. He really does not know where to start with marketing, networking, or taxes.<br>
+
+			<br> is 7:30 PM on April 3, 2018 after a stressful day of work, Nam had very few customers and his cook just let him know he will be moving on and getting a new cooking job
+			with better pay starting next week. Nam is looking at a pile of bills and tax documents in his work office, and knows that if he does not change the way he is running his business
+			he will have to end up having to shut it down.<br>
+
+			<br>Nam decides to start searching online for a business consoultant to help lead him and his business in the right direction.
+
+
 		</main>
 
 
 	</body>
 
 
-</HTML>
+</html>
 
 
---
+++ public_html/documentation/milestone-2a.php
@@ -0,0 +1,71 @@
+<!doctype html>
+
+
+<html>
+
+
+	<head>
+
+		<meta charset="utf-8">
+
+		<title>Robbie Lopez</title>
+
+
+
+
+	</head>
+
+
+	<body>
+
+
+		<header>
+
+		</header>
+
+
+		<main>
+			
+			
+
+			<img src="images/pwp_wireframingd.jpg">
+			
+			<img src="images/pwp_wireframing_about.jpg">
+
+			<img src="images/pwp_wireframing_busiconsult.jpg">
+			
+			<img src="images/pwp_wireframing_webdev.jpg">
+			
+			<img src="images/pwp_wireframing_contact.jpg">
+
+			
+			
+			<img src="images/pwp_wireframingM.jpg">
+			
+			<img src="images/pwp_wireframingM_abo.jpg">
+			
+			<img src="images/pwp_wireframing_webdev.jpg">
+			
+			<img src="images/pwp_wireframingM_busiconsult.jpg">
+			
+			<img src="images/pwp_wireframingM_contact.jpg">
+
+
+
+
+
+
+
+
+
+
+
+		</main>
+
+
+	</body>
+
+
+</html>
+
+
