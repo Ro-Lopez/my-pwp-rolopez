03490bf255a14562bb843785067c08950c751786
Robbie Lopez <rolopez.email@gmail.com>
2017-05-18 16:03:47 -0600

    bootstrap and css files in head

---
+++ public_html/documentation/milestone-2a.php
@@ -0,0 +1,82 @@
+<!doctype html>
+
+
+<html>
+
+
+	<head>
+
+		<meta charset="utf-8">
+
+		<title>Robbie Lopez</title>
+
+
+
+
+	</head>
+
+
+	<body>
+
+
+		<header>
+
+		</header>
+
+
+		<main>
+
+			<h1>PWP Milestone 2</h1>
+
+			<p>This will serve as as the wire framing for my personal website. Clients will come to my site to either get information
+			of my services, find a way to contact me, or get a hold of my resume.</p>
+			
+			<h2>Desktop</h2>
+
+			<img src="../../images/pwp_wireframingd.jpg">
+			
+			<img src="../../images/pwp_wireframing_about.jpg">
+
+			<img src="../../images/pwp_wireframing_contact.jpg">
+
+			<img src="../../images/pwp_wireframing_webdev.jpg">
+
+			<img src="../../images/pwp_wireframing_busiconsult.jpg">
+			
+
+
+			<br><br>
+
+			<h2>Mobile</h2>
+			
+			<p><img src="../../images/pwp_wireframingM.jpg"></p>
+
+			<p><img src="../../images/pwp_wireframingM_abo.jpg"></p>
+
+			<p><img src="../../images/pwp_wireframingM_contact.jpg"></p>
+
+			<p><img src="../../images/pwp_wireframingM_webdev.jpg	"></p>
+
+			<p><img src="../../images/pwp_wireframingM_busiconsult.jpg"></p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+		</main>
+
+
+	</body>
+
+
+</html>
+
+
